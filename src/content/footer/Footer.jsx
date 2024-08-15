932c6e288893ca4a134ecd52d1af7beb64395582
Linuxoid64 <106474005+SemenovNikita2009@users.noreply.github.com>
2024-08-15 19:20:31 +0300

    development-commit-5

--- src/content/footer/Footer.jsx
+++ src/content/footer/Footer.jsx
@@ -17,10 +17,6 @@ function Footer() {
 
             <div className="footer__container-info">
 
-              {/* <p>Телефон - +7(492) 442-23-22</p>
-              <p>Почта - gmail@gmail.com</p>
-              <p>Адрес - Владимирская область, Александров, Институтская улица, 6к5, офис 314, 3 этаж</p> */}
-
               <div className="footer__container-author">
                 <h1>Сайт создан <a target="_blank" href="https://github.com/SemenovNikita2009">Linuxoid86</a></h1>
                 <p>© ООО "ВАШ ПРОЕКТ", {date.getFullYear()}.</p>
